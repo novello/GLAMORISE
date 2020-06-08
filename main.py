b026c8bf73d5dedbca9aec46d7af8ad116862831
Alexandre Novello <alexandre.novello@gruposantaisabel.com.br>
2020-06-08 20:42:34 -0300

    Commit Inicial

---
+++ main.py
@@ -0,0 +1,20 @@
+import glamorise
+import csv
+
+#with open('./datasets/how-questions.csv') as csv_file:
+with open('/content/sample_data/teste.csv') as csv_file:
+  csv_reader = csv.reader(csv_file, delimiter=',', quotechar="'")
+  line_count = 0
+  for row in csv_reader:
+    nl_query = row[0]
+    print("\n\nNatural Language Query: ",nl_query )
+    glamorise = Glamorise(nl_query)
+    glamorise.pattern_scan()
+    glamorise.customized_displacy()
+    print("aggregate function: ", glamorise.aggregate_function)
+    print("aggregate field: ", glamorise.aggregate_field)
+    print("group_by_field: ", glamorise.group_by_field)
+    print("having_field: ", glamorise.having_field)
+    print("group_by: ", glamorise.group_by)
+    print("having: ", glamorise.having)
+    print("cut_text: ", glamorise.cut_text)
