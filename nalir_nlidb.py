c102c95a4a0ab6af1a22632e979fd41ae9dc25b1
Alexandre Novello <alexandre.novello@gmail.com>
2021-01-02 19:01:06 -0300

    3 attemps implementation in NaLIR NLIDB

--- nalir_nlidb.py
+++ nalir_nlidb.py
@@ -28,11 +28,10 @@ class NalirNlidb:
         self.__rdbms = RDBMS(self.__config)
 
     def __change_select(self):
-        fields, result, sql_list = self.__get_fields_in_select()
+        fields, result, sql_list = self.__get_fields_in_sql(self.__sql, '(SELECT )(DISTINCT )?(.*)$', 0, 3, ',')
         all_fields = []
-        transformed_fields = []
-        for field in fields:
-            field = field.strip()
+        transformed_fields = []        
+        for field in fields:            
             if field not in all_fields:            
                 all_fields.append(field)
                 transformed_fields.append(field + ' as ' + field.replace('.', '_').replace('(', '_').replace(')', ''))
@@ -40,11 +39,11 @@ class NalirNlidb:
         for i in range(1, len(sql_list)):
             self.__sql += sql_list[i] + '\n'
 
-    def __get_fields_in_select(self):
-        sql_list = self.__sql.split('\n')
-        result = re.search('(SELECT )(DISTINCT )?(.*)$', sql_list[0], re.IGNORECASE|re.MULTILINE)
-        select = ''
-        fields = result.group(3).split(',')
+    def __get_fields_in_sql(self, sql, regex, sql_list_position, group_num, separator):
+        sql_list = sql.split('\n')
+        result = re.search(regex, sql_list[sql_list_position], re.IGNORECASE|re.MULTILINE)        
+        fields_str = result.group(group_num)
+        fields = [x.strip() for x in fields_str.split(separator)]
         return fields, result, sql_list
 
     def __translate_mysql_datatype_to_sqlite(self, type):
@@ -97,14 +96,14 @@ class NalirNlidb:
             for line in results:
                 if line[0] in nlidb_nql:
                     nlidb_nql = nlidb_nql.replace(line[0], line[1].replace(' ', '_').replace('.', '_'))
-                    nlidb_interface_fields.append(line[1])            
+                    nlidb_interface_fields.add(line[1])            
             return nlidb_nql  
         except Exception as e:
             return nlidb_nql
             #print('Exception: ', e        
 
     def __include_fields(self, nlidb_interface_fields):
-        fields, result, sql_list = self.__get_fields_in_select()        
+        fields, result, sql_list = self.__get_fields_in_sql(self.__sql, '(SELECT )(DISTINCT )?(.*)$', 0, 3, ',')        
         for nlidb_interface_field in nlidb_interface_fields:            
             if nlidb_interface_field not in fields:            
                 fields.append(nlidb_interface_field)                
@@ -115,28 +114,33 @@ class NalirNlidb:
     def execute_query(self, nlq, timer_nlidb_execution_first_and_second_attempt, timer_nlidb_execution_third_attempt, timer_nlidb_json_result_set, nlidb_attempt_level, nlidb_interface_fields):   
         timer_nlidb_execution_first_and_second_attempt.start()     
         columns = result_set = self.__sql = ''
-        sql_first_attempt = sql_second_attempt = sql_third_attempt = ''
+        self.__sql_first_attempt = self.__sql_second_attempt = self.__sql_third_attempt = ''
         try:            
             self.__sql = self.__run_query(nlq)
-            sql_first_attempt = self.__sql
+            self.__sql_first_attempt = self.__sql
 
 
-            if nlidb_attempt_level > 1:
-                self.__include_fields(nlidb_interface_fields)
-                sql_second_attempt = self.__sql
+            if self.__sql:
+                if nlidb_attempt_level > 1:
+                    self.__include_fields(nlidb_interface_fields)
+                    self.__sql_second_attempt = self.__sql
 
-            self.__change_select()            
+                self.__change_select()            
             
             #result_set, cursor_description = self.__SimpleSQLite.execute_sql(sql_result, 'Query executed')
             timer_nlidb_execution_first_and_second_attempt.stop()
             timer_nlidb_json_result_set.start()
             result_set, cursor_description = self.__rdbms.conduct_sql(self.__sql)
 
-            if not result_set and nlidb_attempt_level == 3:
+            if not result_set and nlidb_attempt_level == 3:            
                 timer_nlidb_json_result_set.stop()
                 timer_nlidb_execution_third_attempt.start()
-                result_set, cursor_description = self.__rdbms.conduct_sql(self.__nlq_rebuild()) 
-                sql_third_attempt = self.__sql
+                prior_sql = self.__sql
+                self.__nlq_rebuild(nlidb_interface_fields)
+                self.__sql_third_attempt = self.__sql
+                self.__change_select()
+                if self.__sql != prior_sql:
+                    result_set, cursor_description = self.__rdbms.conduct_sql(self.__sql)                 
                 timer_nlidb_execution_third_attempt.stop()
                 timer_nlidb_json_result_set.start()
             
@@ -149,7 +153,7 @@ class NalirNlidb:
             print("Error processing NLQ in NaLIR: ", nlq)
             print("Exception: ", e)
         finally:                                             
-            return columns, result_set, self.__sql, sql_first_attempt, sql_second_attempt, sql_third_attempt
+            return columns, result_set, self.__sql, self.__sql_first_attempt, self.__sql_second_attempt, self.__sql_third_attempt
 
     def __run_query(self, nlq):
         try:
@@ -173,15 +177,52 @@ class NalirNlidb:
             print("Error processing NLQ in NaLIR: ", nlq)
             print("Exception: ", e)        
 
-    def __nlq_rebuild(self):
+    def __nlq_rebuild(self, nlidb_interface_fields):
         try:
-            nlq = "give me all " + "for each ".join(fields)
+            nlq = "give me all " + " for each ".join(nlidb_interface_fields).replace(".", "_")
             new_sql = self.__run_query(nlq)             
-            self.__sql = self.__join_sql(new_sql)
-            return columns, result_set, self.__sql
+            self.__sql = self.__join_sql(new_sql)            
         except Exception as e:
             print("Error processing second attempt of NLQ in NaLIR: ", nlq)
-            print("Exception: ", e)                                     
+            print("Exception: ", e)      
+
+    def __join_sql(self, new_sql):
+        select_new_sql_fields, select_new_sql_result, new_sql_sql_list = self.__get_fields_in_sql(new_sql, '(SELECT )(DISTINCT )?(.*)$', 0, 3, ',')
+        from_new_sql_fields, from_new_sql_result, new_sql_sql_list = self.__get_fields_in_sql(new_sql, '(FROM )(.*)$', 1, 2, ',')
+        where_new_sql_fields, where_new_sql_result, new_sql_sql_list = self.__get_fields_in_sql(new_sql, '(WHERE )(.*)$', 2, 2, ' AND ')
+
+        select_fields, select_result, sql_list = self.__get_fields_in_sql(self.__sql_second_attempt, '(SELECT )(DISTINCT )?(.*)$', 0, 3, ',')
+        from_fields, from_result, sql_list = self.__get_fields_in_sql(self.__sql_second_attempt, '(FROM )(.*)$', 1, 2, ',')
+        where_fields, where_result, sql_list = self.__get_fields_in_sql(self.__sql_second_attempt, '(WHERE )(.*)$', 2, 2, ' AND ')
+
+        select_final_fields = set()
+        from_final_fields = set()
+        where_final_fields = set()
+
+        for select_new_sql_field in select_new_sql_fields:
+            if select_new_sql_field not in select_fields:
+                select_final_fields.add(select_new_sql_field)
+
+        for from_new_sql_field in from_new_sql_fields:
+            if from_new_sql_field not in from_fields:
+                from_final_fields.add(from_new_sql_field)
+
+        for where_new_sql_field in where_new_sql_fields:
+            if where_new_sql_field not in where_fields:
+                where_final_fields.add(where_new_sql_field)        
+
+        select_final_fields.update(select_fields)
+        from_final_fields.update(from_fields)
+        where_final_fields.update(where_fields)
+
+        select_str = "SELECT DISTINCT " + ', '.join(select_final_fields)
+        from_str = "\nFROM " + ', '.join(from_final_fields)
+        where_str = "\nWHERE " + ' AND '.join(where_final_fields)
+        
+        sql = select_str + from_str + where_str
+        return sql
+
+
             
     
     
