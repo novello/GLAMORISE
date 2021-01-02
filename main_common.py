5fc53ef694d1cdf207fc284b4d4edacffca19d17
Alexandre Novello <alexandre.novello@gmail.com>
2021-01-02 20:30:36 -0300

    adding common print functions to Glamorise class

--- main_common.py
+++ main_common.py
@@ -1,17 +1,5 @@
 from codetiming import Timer
 
-def dump(obj):
-  for attr in dir(obj):
-    # only the properties that are important to print
-    if attr in ['pre_aggregation_functions', 'pre_aggregation_fields', 'pre_group_by_fields',
-                'pre_time_scale_aggregation_functions', 'pre_time_scale_aggregation_fields', 'pre_time_scale_group_by_fields',
-                'pre_having_fields', 'pre_having_conditions', 'pre_having_values', 'pre_having_units',
-                'pre_group_by', 'pre_cut_text', 'pre_replaced_text', 'original_query', 'pre_before_query', 'pre_prepared_query', 'pre_prepared_query_before_field_translation',
-                'nlidb_interface_fields',
-                'pos_aggregation_functions', 'pos_aggregation_fields', 'pos_group_by_fields', 'pos_glamorise_sql', 'pos_nlidb_sql', 'pos_nlidb_sql_first_attempt', 'pos_nlidb_sql_second_attempt', 'pos_nlidb_sql_third_attempt'
-                ] and getattr(obj, attr):
-        print("%s = %r\n" % (attr, getattr(obj, attr)))
-
 def is_jupyter_notebook():
     # check the environment
     try:
@@ -24,45 +12,51 @@ def is_jupyter_notebook():
     except NameError:
         return False      # Probably standard Python interpreter
 
-def printmd(string):
-    display(Markdown(string))
-
-
-def print_results(glamorise, nlq):
+def mc_print(string):
     if jupyter:
-        print("\n\n")
-        mc.printmd("**Natural Language Query**: " + nlq)
+        display(Markdown(string))
     else:
-        print("\n\nNatural Language Query: ",nlq)
+        print(string)
+
+def mc_display(pd):
+    if jupyter:
+        display(pd)
+    else:                
+        print(pd)
+
+
+
+def print_results(glamorise, nlq):    
+    print("\n\n")
+    mc_print("**Natural Language Query**: " + nlq)
 
     glamorise.execute(nlq)
-
+    
     if jupyter:
-        mc.printmd("**spaCy Parse Tree**")
+        mc_print("**spaCy Parse Tree**")
         # show spaCy parse tree
         glamorise.customized_displacy()
-    if jupyter:
-        mc.printmd("**GLAMORISE Internal Properties**")
-    else:
-        print("GLAMORISE Internal Properties")
-    dump(glamorise)
 
-    if jupyter:
-        mc.printmd("**GLAMORISE Result**")
-        # display the result as a pandas dataframe
-        display(glamorise.pd)
-    else:
-        print("GLAMORISE Result")
-        # print the result as a pandas dataframe
-        print(glamorise.pd)
+    print("\n\n")
+    mc_print("GLAMORISE Internal Properties")
+    print("\n")
+    mc_print("GLAMORISE Preprocessor Properties")
+    glamorise.dump('pre_')
 
+    print("\n")
+    mc_print("GLAMORISE NLIDB Interface Properties")
+    glamorise.dump('nlidb_interface_')
 
-    print("timer_pre : {:.2f} sec".format(glamorise._timer_pre.last))
-    print("timer_nlidb_execution_first_and_second_attempt : {:.2f} sec".format(glamorise._timer_nlidb_execution_first_and_second_attempt.last))    
-    print("timer_nlidb_execution_third_attempt : {:.2f} sec".format(glamorise._timer_nlidb_execution_third_attempt.last))           
-    print("timer_nlidb_json_result_set : {:.2f} sec".format(glamorise._timer_nlidb_json_result_set.last))
-    print("timer_pos : {:.2f} sec".format(glamorise._timer_pos.last))
-    print("timer_exibition : {:.2f} sec".format(glamorise._timer_exibition.last))
+    print("\n")
+    mc_print("GLAMORISE Post-processor Properties")
+    glamorise.dump('pos_')
+
+    mc_print("**GLAMORISE Result**")
+    # display the result as a pandas dataframe
+    mc_display(glamorise.pd)    
+
+    glamorise.print_timers()
+    
 
 
 def print_total_timers():
